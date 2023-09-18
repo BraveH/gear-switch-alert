6d79999179ba03793e6544ab6a64e7aa270e693b
MeteoroidShot <ejada.inv@gmail.com>
2023-09-19 01:22:24 +0300

    Removed test plugin accidentally forgotten

--- src/test/java/com/gearswitch/GearSwitchAlertPluginTest.java
+++ src/test/java/com/gearswitch/GearSwitchAlertPluginTest.java
@@ -9,8 +9,7 @@ public class GearSwitchAlertPluginTest
 {
 	public static void main(String[] args) throws Exception
 	{
-		ExternalPluginManager.loadBuiltin(GearSwitchAlertPlugin.class, GearHighlighterPlugin.class,
-				ZebakJugSolverPlugin.class);//, HetSolverPlugin.class);
+		ExternalPluginManager.loadBuiltin(GearSwitchAlertPlugin.class);
 		RuneLite.main(args);
 	}
 }
